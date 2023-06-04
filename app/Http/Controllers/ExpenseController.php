c78303324a8d441f81a1f1ccaec0e00c43c25b96
Filipe Braga <filipe.b.braga@gmail.com>
2023-06-04 10:36:01 -0300

    Create the Expense CRUD Operations

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    $validated = $request->validate([
+        'description' => 'required|max:191',
+        'date' => 'required|date|before_or_equal:today',
+        'amount' => 'required|numeric|min:0',
+    ]);
+
+    $expense = new Expense;
+    $expense->description = $request->description;
+    $expense->date = $request->date;
+    $expense->amount = $request->amount;
+    $expense->user_id = Auth::id();
+    $expense->save();
+
+    // Send an email to the user
+    // \Mail::to(Auth::user()->email)->send(new ExpenseRegistered($expense));
+
+    return response()->json($expense, 201);
+    }
 }

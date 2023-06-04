ca2c0b91119374ea5b7c7965190206b9d4a9fe7c
Filipe Braga <filipe.b.braga@gmail.com>
2023-06-04 16:05:15 -0300

    expenses routes

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,13 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/home', [ExpenseController::class, 'index'])->name('home');
+
+Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+
+Route::get('/expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expenses.edit');
+Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+
+
+
+

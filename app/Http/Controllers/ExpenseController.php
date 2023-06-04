fb71c1016337678a8550091d7f4799916e151110
Filipe Braga <filipe.b.braga@gmail.com>
2023-06-04 16:04:35 -0300

    adding edit update and destroy controller

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -26,7 +26,7 @@ class ExpenseController extends Controller
         // Send an email to the user
         // \Mail::to(Auth::user()->email)->send(new ExpenseRegistered($expense));
 
-        return response()->json($expense, 201);
+        return redirect()->route('home')->with('success', 'Expense added successfully!');
     }
 
     public function index()
@@ -34,5 +34,40 @@ class ExpenseController extends Controller
         $expenses = Expense::where('user_id', Auth::id())->get();
 
         return view('home', compact('expenses'));
-    }   
+    }
+
+    public function edit(Expense $expense)
+    {
+        // Retrieve the expense for editing
+        // You can pass the $expense object to the edit view
+        return view('expenses.edit', compact('expense'));
+    }
+
+    public function update(Request $request, Expense $expense)
+    {
+        // Validate the updated expense data
+        $validated = $request->validate([
+            'description' => 'required|max:191',
+            'date' => 'required|date|before_or_equal:today',
+            'amount' => 'required|numeric|min:0',
+        ]);
+
+        // Update the expense with the new data
+        $expense->description = $request->description;
+        $expense->date = $request->date;
+        $expense->amount = $request->amount;
+        $expense->save();
+
+        // Redirect back to the expenses index page or any other appropriate page
+        return redirect()->route('home')->with('success', 'Expense updated successfully!');
+    }
+
+    public function destroy(Expense $expense)
+    {
+        // Delete the expense
+        $expense->delete();
+
+        // Redirect back to the expenses index page or any other appropriate page
+        return redirect()->route('home')->with('success', 'Expense deleted successfully!');
+    }
 }
